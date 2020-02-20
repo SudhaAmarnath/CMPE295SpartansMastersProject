592594b17c9e29af5bbc93cb233c3e13a723b3c2
SudhaAmarnath <sudha04.a@gmail.com>
2020-02-19 21:28:43 -0800

    user profile page design

---
+++ GrabOn/app/src/main/java/com/spartans/grabon/user/Profile.java
@@ -0,0 +1,16 @@
+package com.spartans.grabon.user;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.spartans.grabon.R;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
