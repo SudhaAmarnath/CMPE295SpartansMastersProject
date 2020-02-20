0da5fab23568bbc3532e76bb201de2026a0f793d
SudhaAmarnath <sudha04.a@gmail.com>
2020-02-19 23:45:16 -0800

    update profile and cart activities

--- GrabOn/app/src/main/java/com/spartans/grabon/cart/Cart.java
+++ GrabOn/app/src/main/java/com/spartans/grabon/cart/Cart.java
@@ -1,10 +1,14 @@
 package com.spartans.grabon.cart;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.spartans.grabon.R;
+import com.spartans.grabon.payment.PaypalPaymentClient;
 
 public class Cart extends AppCompatActivity {
 
@@ -12,5 +16,19 @@ public class Cart extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cart);
+
+        Button cartProceedForPayment;
+
+        cartProceedForPayment = findViewById(R.id.CartProceedForPayment);
+
+        cartProceedForPayment.setOnClickListener(new View.OnClickListener() {
+        @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(), PaypalPaymentClient.class));
+                finish();
+            }
+            }
+        );
+
     }
 }
--- GrabOn/app/src/main/java/com/spartans/grabon/user/Profile.java
+++ GrabOn/app/src/main/java/com/spartans/grabon/user/Profile.java
@@ -1,6 +1,10 @@
 package com.spartans.grabon.user;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,6 +16,7 @@ import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.ListenerRegistration;
+import com.spartans.grabon.Login;
 import com.spartans.grabon.R;
 
 import javax.annotation.Nullable;
@@ -27,6 +32,7 @@ public class Profile extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         final EditText firstname, lastname, email, phone;
+        final Button userLogout;
 
         final FirebaseAuth firebaseAuth;
         FirebaseFirestore firebaseFirestore;
@@ -36,6 +42,7 @@ public class Profile extends AppCompatActivity {
         lastname = findViewById(R.id.Lastname);
         email = findViewById(R.id.Email);
         phone = findViewById(R.id.Phone);
+        userLogout = findViewById(R.id.UserLogout);
 
         firebaseAuth = FirebaseAuth.getInstance();
         firebaseFirestore = FirebaseFirestore.getInstance();
@@ -50,6 +57,20 @@ public class Profile extends AppCompatActivity {
                 lastname.setText(documentSnapshot.getString("lastname"));
                 email.setText(documentSnapshot.getString("email"));
                 phone.setText("+1123456789");
+            }
+        });
+
+        userLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    documentRefRegistration.remove();
+                    FirebaseAuth.getInstance().signOut();//logout
+                } catch (Exception logoutException) {
+                    Log.d(TAG, "logoutException: " + logoutException.toString());
+                }
+                startActivity(new Intent(getApplicationContext(), Login.class));
+                finish();
             }
         });
 
