1a45010a06f84b8f4ca2fc923bdfd154df49f2ca
SudhaAmarnath <sudha04.a@gmail.com>
2020-03-06 09:09:08 -0800

    add firebase storage

--- GrabOn/app/src/main/java/com/spartans/grabon/utils/Singleton.java
+++ GrabOn/app/src/main/java/com/spartans/grabon/utils/Singleton.java
@@ -3,16 +3,18 @@ package com.spartans.grabon.utils;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
 
 /**
  * Author : Sudha Amarnath on 2020-02-20
  */
 public class Singleton {
 
-    // Using Singleton class for FirebaseFirestore and is the entry point for all the user and db operations.
+    // Using Singleton class for Firebase classes and is the entry point for all the user and db operations.
     private static final FirebaseAuth auth = FirebaseAuth.getInstance();
     private static final FirebaseFirestore db = FirebaseFirestore.getInstance();
     private static final FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+    private static final FirebaseStorage storage = FirebaseStorage.getInstance();
 
     public static FirebaseAuth getAuth() {
         return auth;
@@ -24,6 +26,10 @@ public class Singleton {
 
     public static FirebaseUser getUser() {
         return  user;
+    }
+
+    public static FirebaseStorage getStorage() {
+        return storage;
     }
 
 }
