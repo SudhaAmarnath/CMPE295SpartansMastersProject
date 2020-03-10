733e37b07bf313b0d6be18530a47ed5eca458df3
SudhaAmarnath <sudha04.a@gmail.com>
2020-03-09 20:19:38 -0700

    implement item activity

---
+++ GrabOn/app/src/main/java/com/spartans/grabon/interfaces/FileDataStatus.java
@@ -0,0 +1,14 @@
+package com.spartans.grabon.interfaces;
+
+import java.util.ArrayList;
+
+/**
+ * Author : Sudha Amarnath on 2020-03-09
+ */
+public interface FileDataStatus {
+
+    void onSuccess(ArrayList list);
+
+    void onError(String e);
+
+}
