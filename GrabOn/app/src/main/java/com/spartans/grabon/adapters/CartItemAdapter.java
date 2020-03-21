07115f23327269070e96aa56994edcbff4c8a799
SudhaAmarnath <sudha04.a@gmail.com>
2020-03-21 12:51:15 -0700

    add cart item adapters

---
+++ GrabOn/app/src/main/java/com/spartans/grabon/adapters/CartItemAdapter.java
@@ -0,0 +1,77 @@
+package com.spartans.grabon.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.spartans.grabon.R;
+import com.spartans.grabon.model.Item;
+
+import java.util.ArrayList;
+
+/**
+ * Author : Sudha Amarnath on 2020-03-21
+ */
+public class CartItemAdapter extends RecyclerView.Adapter<CartItemAdapter.ViewHolder> {
+
+    private Context context;
+    private ArrayList<Item> items;
+
+    public CartItemAdapter(ArrayList<Item> items, Context context) {
+        this.context = context;
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.layout_cartitems_list,
+                viewGroup, false);
+        return new ViewHolder(v);
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+
+    public int getItemCount() {
+        return items.size();
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int position) {
+        viewHolder.bindModel(items.get(position));
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        Item item;
+        TextView itemName;
+        TextView itemPrice;
+        ImageView itemImage;
+
+        ViewHolder(View v) {
+            super(v);
+            itemName = itemView.findViewById(R.id.layout_cartitems_text);
+            itemPrice = itemView.findViewById(R.id.layout_cartitems_price);
+            itemImage = itemView.findViewById(R.id.layout_cartitems_image);
+        }
+
+        void bindModel(Item items) {
+            this.item = items;
+            itemName.setText(item.getItemName());
+            itemPrice.setText("$"+String.format("%.2f", item.getItemPrice()));
+            Glide.with(context).load(item.getItemImageList().get(0)).into(itemImage);
+        }
+
+    }
+
+}
