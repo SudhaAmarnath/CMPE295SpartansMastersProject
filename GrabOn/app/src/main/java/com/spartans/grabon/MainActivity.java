09568e183b9f8c93cb45979d5277af0f4bbf6fb6
SudhaAmarnath <sudha04.a@gmail.com>
2020-04-26 20:07:27 -0700

    update latitude and longitude

--- GrabOn/app/src/main/java/com/spartans/grabon/MainActivity.java
+++ GrabOn/app/src/main/java/com/spartans/grabon/MainActivity.java
@@ -182,16 +182,24 @@ public class MainActivity extends AppCompatActivity implements RetrieveFeedTask.
             cityDistance.setVisibility(View.VISIBLE);
             locationCity.setText(userCity);
             locationDistance.setText(String.valueOf(distance) + " Miles");
-            userlat = Double.parseDouble(userLatitude);
-            userlon = Double.parseDouble(userLongitude);
+            if (!userLatitude.equals("")) {
+                userlat = Double.parseDouble(userLatitude);
+            }
+            if(!userLongitude.equals("")) {
+                userlon = Double.parseDouble(userLongitude);
+            }
             usermiles = distance;
         } else {
             if (currentUserCity.equals("") == false) {
                 cityDistance.setVisibility(View.VISIBLE);
                 locationCity.setText(currentUserCity);
                 locationDistance.setText(String.valueOf(distance) + " Miles");
-                userlat = Double.parseDouble(userLatitude);
-                userlon = Double.parseDouble(userLongitude);
+                if (!userLatitude.equals("")) {
+                    userlat = Double.parseDouble(userLatitude);
+                }
+                if(!userLongitude.equals("")) {
+                    userlon = Double.parseDouble(userLongitude);
+                }
                 usermiles = distance;
             }
         }
@@ -217,8 +225,12 @@ public class MainActivity extends AppCompatActivity implements RetrieveFeedTask.
                     cityDistance.setVisibility(View.VISIBLE);
                     locationCity.setText(userCity);
                     locationDistance.setText(String.valueOf(distance) + " Miles");
-                    userlat = Double.parseDouble(userLatitude);
-                    userlon = Double.parseDouble(userLongitude);
+                    if (!userLatitude.equals("")) {
+                        userlat = Double.parseDouble(userLatitude);
+                    }
+                    if(!userLongitude.equals("")) {
+                        userlon = Double.parseDouble(userLongitude);
+                    }
                     usermiles = distance;
                     refreshItems();
                 }
